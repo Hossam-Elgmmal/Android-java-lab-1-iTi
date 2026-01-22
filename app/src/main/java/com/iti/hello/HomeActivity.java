6e4447076a661871aff03e1f11f41a9ec95e3f5e
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-01-22 09:59:53 +0200

    Added Saving to shared preferences

--- app/src/main/java/com/iti/hello/HomeActivity.java
+++ app/src/main/java/com/iti/hello/HomeActivity.java
@@ -1,9 +1,7 @@
 package com.iti.hello;
 
-import static com.iti.hello.MainActivity.NAME_KEY;
-import static com.iti.hello.MainActivity.PHONE_KEY;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,6 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String PREFERENCES_NAME = "my_preferences";
+
+    public static final String NAME_KEY = "username";
+    public static final String PHONE_KEY = "phone";
+
+    private TextView usernameTextView;
+    private TextView phoneTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +36,14 @@ public class HomeActivity extends AppCompatActivity {
         String name = getIntent().getStringExtra(NAME_KEY);
         String number = getIntent().getStringExtra(PHONE_KEY);
 
-        TextView username = findViewById(R.id.username);
-        TextView phone = findViewById(R.id.phoneNumber);
+        usernameTextView = findViewById(R.id.username);
+        phoneTextView = findViewById(R.id.phoneNumber);
 
         if (name != null) {
-            username.setText(name);
+            usernameTextView.setText(name);
         }
         if (number != null) {
-            phone.setText(number);
+            phoneTextView.setText(number);
         }
 
         ((Toolbar) findViewById(R.id.topBar)).setNavigationOnClickListener(v -> finish());
@@ -47,6 +53,65 @@ public class HomeActivity extends AppCompatActivity {
                     Intent intent = new Intent(this, LinearActivity.class);
                     startActivity(intent);
                 });
+
+        findViewById(R.id.addToSharedPreferencesBtn)
+                .setOnClickListener(v -> addToSharedPreferences());
+        findViewById(R.id.getFromSharedPreferencesBtn)
+                .setOnClickListener(v -> getFromSharedPreferences());
+        findViewById(R.id.addToInternalMemoryBtn)
+                .setOnClickListener(v -> addToInternalMemory());
+        findViewById(R.id.getFromInternalMemoryBtn)
+                .setOnClickListener(v -> getFromInternalMemory());
+        findViewById(R.id.addToSqliteBtn)
+                .setOnClickListener(v -> addToSqlite());
+        findViewById(R.id.getFromSqliteBtn)
+                .setOnClickListener(v -> getFromSqlite());
+
+    }
+
+    private void addToSharedPreferences() {
+        String name = usernameTextView.getText().toString();
+        String number = phoneTextView.getText().toString();
+        SharedPreferences sharedPrefs = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(NAME_KEY, name);
+        editor.putString(PHONE_KEY, number);
+        editor.apply();
+        usernameTextView.setText("");
+        phoneTextView.setText("");
+    }
+
+    private void getFromSharedPreferences() {
+        SharedPreferences sharedPrefs = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+        String name = sharedPrefs.getString(NAME_KEY, "");
+        String number = sharedPrefs.getString(PHONE_KEY, "");
+        usernameTextView.setText(name);
+        phoneTextView.setText(number);
+    }
+
+    private void addToInternalMemory() {
+        String name = usernameTextView.getText().toString();
+        String number = phoneTextView.getText().toString();
+
+
+
+        usernameTextView.setText("");
+        phoneTextView.setText("");
+    }
+    private void getFromInternalMemory() {
+
+    }
+
+    private void addToSqlite() {
+        String name = usernameTextView.getText().toString();
+        String number = phoneTextView.getText().toString();
+
+
+
+        usernameTextView.setText("");
+        phoneTextView.setText("");
+    }
+    private void getFromSqlite() {
 
     }
 }
