6aba8c151ce96a2848a6fb91ba6fcae3620f13a5
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-01-22 10:34:45 +0200

    Added Saving to internal storage

--- app/src/main/java/com/iti/hello/HomeActivity.java
+++ app/src/main/java/com/iti/hello/HomeActivity.java
@@ -3,18 +3,27 @@ package com.iti.hello;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public static final String TAG = "HomeActivityX";
+
     public static final String PREFERENCES_NAME = "my_preferences";
+    public static final String INTERNAL_FILE_NAME = "my_file";
 
     public static final String NAME_KEY = "username";
     public static final String PHONE_KEY = "phone";
@@ -93,13 +102,51 @@ public class HomeActivity extends AppCompatActivity {
         String name = usernameTextView.getText().toString();
         String number = phoneTextView.getText().toString();
 
+        JSONObject jsonObject = new JSONObject();
 
+        try {
+            jsonObject.put(NAME_KEY, name);
+            jsonObject.put(PHONE_KEY, number);
+        } catch (JSONException e) {
+            Log.e(TAG, "addToInternalMemory: unable to make json", e);
+            return;
+        }
+        try (FileOutputStream output = openFileOutput(INTERNAL_FILE_NAME, MODE_PRIVATE)) {
 
+            output.write(jsonObject.toString().getBytes());
+
+        } catch (IOException e) {
+            Log.e(TAG, "addToInternalMemory: unable to write to file", e);
+            return;
+        }
         usernameTextView.setText("");
         phoneTextView.setText("");
     }
-    private void getFromInternalMemory() {
 
+    private void getFromInternalMemory() {
+        try (FileInputStream input = openFileInput(INTERNAL_FILE_NAME)) {
+
+            int length = input.available();
+            byte[] bytes = new byte[length];
+            input.read(bytes);
+
+            String json = new String(bytes);
+
+            JSONObject jsonObject = new JSONObject(json);
+
+            String name = jsonObject.getString(NAME_KEY);
+            String number = jsonObject.getString(PHONE_KEY);
+
+            usernameTextView.setText(name);
+            phoneTextView.setText(number);
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "getFromInternalMemory: file not found", e);
+        } catch (IOException e) {
+            Log.e(TAG, "getFromInternalMemory: io exception", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "getFromInternalMemory: unable to parse json", e);
+        }
     }
 
     private void addToSqlite() {
@@ -107,10 +154,10 @@ public class HomeActivity extends AppCompatActivity {
         String number = phoneTextView.getText().toString();
 
 
-
         usernameTextView.setText("");
         phoneTextView.setText("");
     }
+
     private void getFromSqlite() {
 
     }
